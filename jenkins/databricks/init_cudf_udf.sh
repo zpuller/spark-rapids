edede7c444d9692f3cfe4f99ddfa313e74e7ec97
Jenkins Automation <70000568+nvauto@users.noreply.github.com>
2026-07-27 16:28:55 +0800

    Bump up version to 26.10 (#15387)
    Keep the rapids JNI, private and hybrid dependency version at release
    version until the nightly CI for 26.10 is complete. Track the dependency
    update process at: https://github.com/NVIDIA/cudf-spark/issues/15386
    
    
    
    Documentation
    - [ ] Updated for new or modified user-facing features or behaviors
    - [X] No user-facing change
    
    Testing
    - [ ] Added or modified tests to cover new code paths
    - [ ] Covered by existing tests
    (Please provide the names of the existing tests in the PR description.)
    - [X] Not required
    
    Performance
    - [ ] Tests ran and results are added in the PR description
    - [ ] Issue filed with a link in the PR description
    - [X] Not required
    
    Signed-off-by: nvauto <70000568+nvauto@users.noreply.github.com>

--- jenkins/databricks/init_cudf_udf.sh
+++ jenkins/databricks/init_cudf_udf.sh
@@ -20,7 +20,8 @@
 
 set -ex
 
-CUDF_VER=${CUDF_VER:-26.08.0-SNAPSHOT}
+# TODO: https://github.com/NVIDIA/cudf-spark/issues/15386
+CUDF_VER=${CUDF_VER:-26.10.0-SNAPSHOT}
 CUDA_VER=${CUDA_VER:-12.9}
 
 # Need to explicitly add conda into PATH environment, to activate conda environment.
